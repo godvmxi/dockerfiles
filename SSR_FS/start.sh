36f6d976e0c7121d148e058320b247c03bd73f68
JimLee1996 <59061463@qq.com>
2016-11-29 20:12:25 +0800

    Add files via upload

--- SSR_FS/start.sh
+++ SSR_FS/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
+nohup /usr/sbin/sshd -D &
+nohup /usr/bin/python /ssr/shadowsocks/server.py -c /shadowsocks.json > /dev/null &
 /etc/init.d/finalspeed start
-/usr/bin/python /ssr/shadowsocks/server.py -c /shadowsocks.json > /dev/null
-
