1e85b7c06c9ed56dfaff2d791f31fd9f8817e539
JimLee1996 <59061463@qq.com>
2016-12-28 18:31:44 +0800

    Modified, Thx to ruyo.net

--- ICSSR/server.js
+++ ICSSR/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var superagent = require('superagent');
 var ejs = require('ejs');
-var app =   express();
+var app = express();
 app.use(express.static(__dirname + '/public'));
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
@@ -20,13 +20,13 @@ if(args[0].indexOf('@') > -1)
 
 
 appid   =   args[2] || 'all',
-images  =   ["malaohu/ssr-with-net-speeder","lowid/ss-with-net-speeder","smounives/shadowsocksr-docker"];
+images  =   ["jimlee1996/ssr"];
 
 
 app.get('/', function(req, res) {
     getit(appid,function(err,data){   
         if(err || !data)
-            res.send('没有查询到数据。请检查node启动参数是否正确。更多内容请访问：https://github.com/malaohu/ssr-with-net-speeder/tree/arukas');
+            res.send('没有查询到数据。请检查node启动参数是否正确');
         else    
             res.render('./index.html',{"data":data || []});
     })
@@ -80,35 +80,13 @@ function deal_data(_appid,data,callback)
                 continue;
             for (var j = 0; j < jn.attributes.port_mappings.length; j++)
             {
-                var host = jn.attributes.port_mappings[j][0].host;    
+                var host = jn.attributes.port_mappings[j][0].host;
                 var ip = host.substring(6,host.indexOf(".")).replace(/-/g,".");
                 var service_port = jn.attributes.port_mappings[j][0].service_port;
                 var container_port = jn.attributes.port_mappings[j][0].container_port;
-                var cmd = jn.attributes.cmd;
-                var ss_method = '',ss_password = '',ss_port = '',ss_protocol = '',ss_obfs = '';
-                //try to get ss method
-                if(/-m\s+([^ ]+)/.test(cmd))
-                    ss_method = RegExp.$1;
-                //try to get ss password
-                if(/-k\s+([^ ]+)/.test(cmd))
-                    ss_password = RegExp.$1;
-                //try to get ss port
-                if(/-p\s+([^ ]+)/.test(cmd))
-                    ss_port = RegExp.$1;
-                //try to get ssr protocol 
-                if(/-O\s+([^ ]+)/.test(cmd))
-                    ss_protocol = RegExp.$1;
-                //try to get ssr obfs 
-                if(/-o\s+([^ ]+)/.test(cmd))
-                    ss_obfs = RegExp.$1;
                 if(ss_port == container_port)
                 {
-                    var ret_json = {"appid":data[i].id,"server":ip,"server_port":service_port,"password":ss_password,"method":ss_method};
-                    if(ss_protocol && ss_obfs)
-                    {
-                        ret_json["protocol"] = ss_protocol;
-                        ret_json["obfs"] = ss_obfs;
-                    }
+                    var ret_json = {"appid":data[i].id,"server":ip,"server_port":service_port};                    
                     ret_list.push(ret_json);
                 }
             }
@@ -127,9 +105,6 @@ app.get('/:appid',function(req,res){
    }); 
 })
 
-app.get('/i', function (req, res) {
-    res.send('http://51.ruyo.net');
-})
 
 app.listen(3999, function () {
   console.log('Example app listening on port 3999')
